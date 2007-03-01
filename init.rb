90494881044ce214a329c7b4f7f6e26e7468e2b0
aaronp <aaronp@57d87cb8-d8f3-44f2-a25c-9dac74195044>
2007-03-01 21:01:41 +0000

    adding initial version of trigger happy
    git-svn-id: svn://rubyforge.org/var/svn/artriggers/trunk/trigger_happy@1 57d87cb8-d8f3-44f2-a25c-9dac74195044

---
+++ init.rb
@@ -0,0 +1,2 @@
+require 'active_record/schema_dumper'
+require 'trigger_migrations'
---
+++ lib/trigger_migrations.rb
@@ -0,0 +1,50 @@
+module ActiveRecord
+  class SchemaDumper
+    private
+    alias trigger_old_tables tables
+    def tables(stream)
+      trigger_old_tables(stream)
+      @connection.tables.sort.each do |tbl|
+        next if tbl == 'schema_info'
+        triggers(tbl, stream)
+      end
+    end
+
+    def triggers(table, stream)
+      triggers = @connection.triggers(table)
+      triggers.each do |trigger|
+        stream.print "  add_trigger \"#{trigger.name}\", :on => \"#{trigger.reference_table}\", :timing => \"#{trigger.timing}\", :event => \"#{trigger.event}\", :statement => \"#{trigger.statement}\""
+        stream.puts
+      end
+    end
+  end
+
+  module ConnectionAdapters
+    class TriggerDefinition < Struct.new(:name, :event, :reference_table,
+                                         :statement, :timing, :created,
+                                         :sql_mode, :definer
+                                        )
+    end
+    class MysqlAdapter < AbstractAdapter
+      def triggers(table, name = nil)
+        triggers = []
+        execute("SHOW TRIGGERS LIKE '#{table}'", name).each do |row|
+          triggers <<  TriggerDefinition.new(*row)
+        end
+
+        triggers
+      end
+    end
+
+    module SchemaStatements
+      def add_trigger(name, opts = {})
+        sql = "CREATE TRIGGER #{name} #{opts[:timing]} #{opts[:event]} ON #{opts[:on]} FOR EACH ROW #{opts[:statement]}"
+        execute sql
+      end
+
+      def drop_trigger(name)
+        execute("DROP TRIGGER #{name}")
+      end
+    end
+  end
+end
